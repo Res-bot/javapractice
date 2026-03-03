1233ae96435ff9f63939faa28d08d32037938ad2
Reshma <reshmamahanty53@gmail.com>
2026-03-03 19:45:43 +0530

    kadane's algorithm for maximum subarray

--- max_subarray.java
+++ max_subarray.java
@@ -1,5 +1,6 @@
 public class max_subarray {
 
+    //naive approach
     public int maxSubArray(int[] nums) {
         int max_sum = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -12,9 +13,26 @@ public class max_subarray {
         return max_sum;
     }
 
+    //Kadane's Algorithm
+    public int maxSubArr2(int[] nums2){
+        int sum2 = nums2[0];
+        int max_sum2 = nums2[0];
+        for (int i = 0; i < nums2.length; i++) {
+            if(sum2>nums2[i]){
+                sum2 = sum2+nums2[i];
+            }else{
+                sum2 = nums2[i];
+            }if (sum2 > max_sum2) {
+                max_sum2 = sum2;
+            }
+        }
+        return max_sum2;
+    }
+
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
         max_subarray marr = new max_subarray();
         System.out.println( marr.maxSubArray(arr));
+        System.out.println(marr.maxSubArr2(arr));
     }
 }
